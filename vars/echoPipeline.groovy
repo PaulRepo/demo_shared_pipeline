13667a44820b600fb6b53a079350d3b25437c6e9
PaulRepo <54524833+PaulRepo@users.noreply.github.com>
2021-04-18 11:05:07 +0530

    Create echoPipeline.groovy

---
+++ vars/echoPipeline.groovy
@@ -0,0 +1,16 @@
+def call(body) {
+  def config = [:]
+  body.resolveStrategy = Clouse.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  pipeline {
+    agent any
+    stages {
+      stage('Echo from the other side') {
+        steps {
+          echo "${config.message}"
+        }
+      }
+    }
+  }
+}
